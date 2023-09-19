51e410551a2cf21f669d11ad3c2935ca0a28f021
Belba Jurgen <124259718+jurrbl@users.noreply.github.com>
2023-09-19 12:08:47 +0200

    Main form edited (Look & Feel) / Result Label

--- Calculator-2023/Form1.Designer.cs
+++ Calculator-2023/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator_2023
 {
-    partial class Form1
+    partial class Calculator
     {
         /// <summary>
         /// Variabile di progettazione necessaria.
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblResult
+            // 
+            this.lblResult.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI Semibold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(0, 50);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(324, 80);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "0";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(324, 511);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.Text = "Calculator";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
